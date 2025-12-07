c663ec5190a14dc05e170e25349c495e63dc8a6f
artem-totality <artem.totality@gmail.com>
2025-12-07 22:08:35 +0000

    Add AccountNotFoundException

--- src/ie/atu/agile/junit/BankingException.java
+++ src/ie/atu/agile/junit/BankingException.java
@@ -5,3 +5,16 @@ public class BankingException extends Exception {
 		super(message);
 	}
 }
+
+class AccountNotFoundException extends BankingException {
+	private String accountHolder;
+
+	public AccountNotFoundException(String accountHolder) {
+		super("Account not found: " + accountHolder);
+		this.accountHolder = accountHolder;
+	}
+
+	public String getAccountHolder() {
+		return this.accountHolder;
+	}
+}
