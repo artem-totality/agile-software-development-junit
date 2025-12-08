93d60050a68fc024014771b05fc924ecee8d19d8
artem-totality <artem.totality@gmail.com>
2025-12-08 00:25:04 +0000

    Add test checkAccountCreatingValues

--- src/ie/atu/agile/junit/AccountTest.java
+++ src/ie/atu/agile/junit/AccountTest.java
@@ -2,6 +2,7 @@ package ie.atu.agile.junit;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,10 @@ class AccountTest {
 	}
 
 	@Test
-	void test() {
+	void checkAccountCreatingValues() {
+		Assertions.assertEquals(this.account.getAccountHolder(), "Artem");
+		Assertions.assertEquals(this.account.getBalance(), 500);
+		Assertions.assertEquals(this.account.getLoan(), 0);
 	}
 
 }
