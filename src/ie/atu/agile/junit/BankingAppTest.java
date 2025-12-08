138fc1544de5c8549db6b6338e58aedf7c714bd2
artem-totality <artem.totality@gmail.com>
2025-12-08 01:42:28 +0000

    Add BankingAppTest class

---
+++ src/ie/atu/agile/junit/BankingAppTest.java
@@ -0,0 +1,31 @@
+package ie.atu.agile.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BankingAppTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+	}
+
+}
